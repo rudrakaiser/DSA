950aa5e26c19dc352aa9318af8c8ebb99458e7f6
rudra-404 <rudrakaiser840@gmail.com>
2025-05-06 16:36:56 +0600

    Primality Test

--- Number Theory/Primality Test/primality_test.cpp
+++ Number Theory/Primality Test/primality_test.cpp
@@ -1,37 +1,20 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 bool primality_test (int n) {
-    if (n == 2 || n == 3) {
-        return true;
-    } else if ((n & 1) == 0 || n < 2) {
-        return false;
-    } else {
-        for (int i = 3; i * i <= n; i += 2) {
-            if (n % i == 0) {
-                return false;
-            }
+    if (n < 2) return false;
+    else if (n <= 3) return true;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return false;
         }
-    }
-    
-    return true;
+    } return true;
 }
 
 int main () {
-    int test_case;
-    cout << "How many numbers do you want to check >>> ";
-    cin >> test_case;
-
-    while (test_case--) {
-        int num;
-        cout << "Enter A Number >>> ";
-        cin >> num;
-        if ( primality_test(num) ) {
-            cout << "PRIME NUMBER" << endl;
-        } else {
-            cout << "NOT PRIME NUMBER" << endl;
-        }
-    }
-
+    int n;
+    cin >> n;
+    if ( primality_test(n) ) cout << "YES";
+    else cout << "NO";
     return 0;
 }
